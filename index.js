8767975278580ffd57fbf2d1f2b427556d194119
Kiara Johnson <johns.kia35@gmail.com>
2022-11-03 15:33:57 -0600

    REST API

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
+const members = require('./Members');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-})
+//Gets all members
+app.get('/api/members', (req, res) => res.json(members));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.Port || 5000;
 
